149a5894307fb505f3b70959bf46d8cd4c416fdd
Vaishnavi-7338 <121495945+Vaishnavi-7338@users.noreply.github.com>
2023-12-05 19:53:16 +0530

    Create Easy1.py

---
+++ Easy1.py
@@ -0,0 +1,14 @@
+# Define a string variable 's'
+s = "luffy is still joyboy"
+
+# Use the split() method to split the string into a list of words
+g = s.split()
+
+# Access the last element of the list using index -1
+last_word = g[-1]
+
+# Use the len() function to get the length of the last word
+length_of_last_word = len(last_word)
+
+# Print the result
+print(length_of_last_word)
